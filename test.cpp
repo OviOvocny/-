5cd500b4d18424fa7f32b6ed50cb2fb53d6c9437
OviOvocny <werint.pp@gmail.com>
2021-11-03 23:45:37 +0100

    velký přídavek

--- test.cpp
+++ test.cpp
@@ -5,6 +5,29 @@ používej jmenný_prostor std;
 
 celé_číslo hlavní(prázdnota)
 {
-	kven << "Č++ je nejlepší na světě";
+	neoznaménkovaný znak výpis = 'c';
+	bool dobrý_výpis = výpis == 'č';
+
+	pokud (dobrý_výpis == lež) {
+		výpis = 'č';
+	} jinak {
+		kven << "Očekával jsi C++, ale bylo to Č++!" << konecř;
+	}
+
+	přepínač (výpis) {
+		šuplík 'c':
+			kchyb << "C++ je nejangličtější na světě" << konecř;
+			rozbít;
+		šuplík 'č':
+			kven << "Č++ je nejlepší na světě" << konecř;
+			rozbít;
+	}
 	vrať 0;
 }
+
+třída Tř {
+	veřejný:
+		nehybný dlouhý dvojitý číslo() {
+			vrať 4.0;
+		}
+};
--- č.h
+++ č.h
@@ -1,8 +1,60 @@
 #pragma once
+//
+// typy
+#define prázdnota void
+//
+#define krátký short
+#define dlouhý long
+#define neoznaménkovaný unsigned
+#define podepsaný signed
+//
+#define celé_číslo int
+#define plovák float
+#define dvojitý double
+#define znak char
+//
+#define sjednocení union
+#define očísl enum
+#define strukt struct
+#define třída class
+//
+// klíčová slova
+#define pravda true
+#define lež false
+#define vrať return
+#define tohle this
 #define používej using
 #define jmenný_prostor namespace
-#define celé_číslo int
-#define hlavní main
-#define prázdnota void
+#define nový new
+#define smazat delete
+//
+#define veřejný public
+#define soukromý private
+#define chráněný protected
+#define nehybný static
+#define šablona template
+#define návrh concept
+//
+// řízení toku
+#define pokud if
+#define jinak else
+#define přepínač switch
+#define šuplík case
+#define rozbít break
+#define pokračovat continue
+#define zkus try
+#define chyť catch
+#define hoď throw
+#define pro for
+#define dokud while
+#define dělej do
+//
+// iostream
 #define kven cout
-#define vrať return
+#define kdovnitř cin
+#define kchyb cerr
+#define kzáznam clog
+#define konecř endl
+//
+// další ptákoviny
+#define hlavní main
