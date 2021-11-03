8bf033724e22ad9471d6097e41cb97f5446968c3
OviOvocny <werint.pp@gmail.com>
2021-11-03 22:40:53 +0100

    přidat č++ zdroje

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Č.h"
+
+Používej Jmenný_prostor std;
+
+Celé_číslo Hlavní(Prázdnota)
+{
+	Kven << "Č++ je nejlepší na světě";
+	Vrať 0;
+}
---
+++ č.h
@@ -0,0 +1,8 @@
+#pragma once
+#define Používej using
+#define Jmenný_prostor namespace
+#define Celé_číslo int
+#define Hlavní main
+#define Prázdnota void
+#define Kven cout
+#define Vrať return
